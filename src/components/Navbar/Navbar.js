c63502d719e00c1b4ab43d0c53aa30dfb7d837a2
Philip <philipokedi@gmail.com>
2022-04-20 23:53:56 +0100

    fix: nav link function

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Link from 'next/link';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 
@@ -11,49 +11,50 @@ export default function Nav() {
 
 
     const handleClick = () => setClick(!click);
-    return ( 
+    const handleToggle = () => setClick(false);
+    return (
         <nav className='navbar'>
-            
-            <div className="navContainer"> 
-             <div className="mobileMenuIcon"
-            onClick={handleClick}
-            >
-               <i className={`fas fa-bars ${click ? 'fas fa-times' : ''}`}></i>
-              
+
+            <div className="navContainer">
+                <div className="mobileMenuIcon"
+                    onClick={handleClick}
+                >
+                    <i className={`fas fa-bars ${click ? 'fas fa-times' : ''}`}></i>
+
+
+                </div>
+
+                <ul className={click ? 'active' : 'navMenu'}>
+                    <li onClick={handleToggle} className='li'>
+                        <Link className="link" href="/#home" >Home</Link>
+                    </li>
+                    <li onClick={handleToggle} className='li'>
+                        <Link className="link" href="/#about" >About</Link>
+                    </li>
+                    <li onClick={handleToggle} className='li'>
+                        <Link className="link" href="/#experience" >Experience</Link>
+                    </li>
+                    <li onClick={handleToggle} className='li'>
+                        <Link className="link" href="/#projects" >Projects</Link>
+                    </li>
+                    <li onClick={handleToggle} className='li'>
+                        <Link className="link" href="/#contact" >Contact</Link>
+                    </li>
+                    <div className="socials">
+
+
+                        <Link href="https://github.com/fellipxd" passHref aria-label="GitHub" target="_blank" rel="noreferrer"><i className="fab fa-github social-icons"></i></Link>
+                        <Link href="mailto:philipokedi@gmail.com" passHref aria-label="Linkedin" target="_blank" rel="noreferrer"><i className="far fa-envelope social-icons"></i></Link>
+                        <Link href="https://www.linkedin.com/in/philip-okedi/" passHref aria-label="Linkedin" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in social-icons"></i></Link>
+
+
+                    </div>
+
+                </ul>
 
             </div>
-        
-            <ul className={click ? 'active' : 'navMenu'}>
-            <li onClick={handleClick} className='li'>
-                <Link  className="link" href="/#home" >Home</Link>
-            </li>
-            <li onClick={handleClick} className='li'>
-                <Link  className="link" href="/#about" >About</Link>
-            </li>
-            <li onClick={handleClick} className='li'>
-                <Link  className="link" href="/#experience" >Experience</Link>
-            </li>
-            <li onClick={handleClick} className='li'>
-                <Link  className="link" href="/#projects" >Projects</Link>
-            </li>
-            <li onClick={handleClick} className='li'>
-                <Link className="link" href="/#contact" >Contact</Link>
-            </li>
-           <div className="socials">
-          
-          
-            <Link  href="https://github.com/fellipxd" passHref aria-label="GitHub" target="_blank" rel="noreferrer"><i className="fab fa-github social-icons"></i></Link>
-                   <Link   href="mailto:philipokedi@gmail.com" passHref aria-label="Linkedin" target="_blank" rel="noreferrer"><i className="far fa-envelope social-icons"></i></Link>
-                   <Link href="https://www.linkedin.com/in/philip-okedi/" passHref aria-label="Linkedin" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in social-icons"></i></Link>
-                 
-                  
-           </div>
-            
-        </ul>
-       
-            </div>
-        
-      
-    </nav>
+
+
+        </nav>
     )
 }
