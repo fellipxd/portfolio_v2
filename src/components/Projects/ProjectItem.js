748963e282291db2bb43e68f796336838bcdc0a0
Philip <philipokedi@gmail.com>
2022-04-20 23:56:50 +0100

    fix: project card link

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -12,19 +12,23 @@ export default function ProjectItem() {
         <div className="grid gap-6 row-gap-5 lg:grid-cols-2"  >
             {projects.map((project, index) =>
                 <div key={project?.name || idx} className="lg:h-half mx-auto md:h-half h-quart w-11/12  rounded-lg shadow-xl bg-mantis-150 ">
+                    {/* <Link href={`/${project.name}`} passHref rel="noreferrer" > */}
                     <div className='rounded-lg h-view relative  '>
                         <div className='absolute rounded-t-lg h-full w-full bg-black bg-opacity-60 z-10 order-first transform  hover:bg-transparent transition duration-700 ease-out' ></div>
+
                         <img
                             className="w-full h-full rounded-t-lg  "
                             src={project?.img || ""}
                             alt={project?.alt || ""}
                         />
                     </div>
+                    {/* </Link> */}
                     <div className='py-4 px-8 relative h-2/5 '>
-
-                        <h5 className="-mb-8 text-xl  font-bold leading-none sm:text-2xl text-mantis-500 hover:text-mantis-700  cursor-pointer">
-                            {project?.name || ""}
-                        </h5>
+                        <Link href={`/${project.name}`} passHref rel="noreferrer" >
+                            <h5 className="-mb-8 text-xl  font-bold leading-none sm:text-2xl text-mantis-500 hover:text-mantis-700  cursor-pointer">
+                                {project?.name || ""}
+                            </h5>
+                        </Link>
 
                         <p className='text-sm lg:text-base md:text-base mb-4'>
                             {truncateText(project?.desc)}
